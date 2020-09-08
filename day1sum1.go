faf39da50f5f6ab848176109b4d503c78157d794
snipperbytes <snipperbytes@gmail.com>
2020-09-08 12:05:03 +0530

    initial commit 1

---
+++ day1sum1.go
@@ -0,0 +1,31 @@
+package main
+
+import ("fmt";"os";"strconv")
+
+func main(){
+
+	inputData := os.Args
+	if (len(inputData) < 5){
+		fmt.Println("Please enter in below format\nExample : 10 3 | -\n")
+		os.Exit(1)
+	}
+
+	num1,_ := strconv.Atoi(os.Args[1])
+	num2,_ := strconv.Atoi(os.Args[2])
+	for i:=0;i<=num1;i++{
+		if(os.Args[3] == "n"){
+			fmt.Print(i)
+		}else{
+			fmt.Print(os.Args[3])
+		}
+		if(i == num1){
+			fmt.Print("\n")
+			break
+		}
+		for j:=0;j<num2;j++{
+			fmt.Print(os.Args[4])
+		}
+	}
+
+
+}
