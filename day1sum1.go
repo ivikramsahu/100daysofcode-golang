42ec5a4c5f1a04e77cff263829c80207aba57b20
snipperbytes <snipperbytes@gmail.com>
2020-09-08 12:11:10 +0530

    removed unnecessary variables

--- day1sum1.go
+++ day1sum1.go
@@ -4,28 +4,25 @@ import ("fmt";"os";"strconv")
 
 func main(){
 
-	inputData := os.Args
-	if (len(inputData) < 5){
-		fmt.Println("Please enter in below format\nExample : 10 3 | -\n")
-		os.Exit(1)
-	}
+    if (len(os.Args) < 5){
+        fmt.Printf("Please enter in below format\nExample : 10 3 | -\n")
+        os.Exit(1)
+    }
 
-	num1,_ := strconv.Atoi(os.Args[1])
-	num2,_ := strconv.Atoi(os.Args[2])
-	for i:=0;i<=num1;i++{
-		if(os.Args[3] == "n"){
-			fmt.Print(i)
-		}else{
-			fmt.Print(os.Args[3])
-		}
-		if(i == num1){
-			fmt.Print("\n")
-			break
-		}
-		for j:=0;j<num2;j++{
-			fmt.Print(os.Args[4])
-		}
-	}
-
-
+    num1,_ := strconv.Atoi(os.Args[1])
+    num2,_ := strconv.Atoi(os.Args[2])
+    for i:=0;i<=num1;i++{
+        if(os.Args[3] == "n"){
+            fmt.Print(i)
+        }else{
+            fmt.Print(os.Args[3])
+        }
+        if(i == num1){
+            fmt.Print("\n")
+            break
+        }
+        for j:=0;j<num2;j++{
+            fmt.Print(os.Args[4])
+        }
+    }
 }
